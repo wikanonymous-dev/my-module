8716e9c11c8d6f356f37bbdb39efdef160db72c3
alam <alam@privyid.tech>
2023-02-07 13:34:31 +0700

    fix: build config external deps

--- build.config.ts
+++ build.config.ts
@@ -3,6 +3,7 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: ['./src/index'],
   declaration: true,
+  externals: ['#imports', '#app'],
   rollup: {
     emitCJS: true,
     cjsBridge: false,
