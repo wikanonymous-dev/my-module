d567ec9f9e0b97d5644674cb1df360f4348d0e31
alam <alam@privyid.tech>
2023-02-07 14:03:56 +0700

    fix: remove alias inside build module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { useCookie } from '#imports'
-
 export function runConsole() {
-  const session = useCookie('session/token')
-
-  console.log('Console running...' + session)
+  console.log('Console running...')
 }
